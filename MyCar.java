5f94ed976b2068f65299ab45a92b929f1742194b
suganya65 <105557985+suganya65@users.noreply.github.com>
2022-05-15 07:56:03 +0100

    Add files via upload

---
+++ MyCar.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class MyCar {
+	
+
+	public static void main(String[] args) {
+
+		Car myCar1 = new Car();
+		
+		myCar1.applyBreak();
+		myCar1.applyGear();
+		myCar1.switchonAc();
+		myCar1.applyAcclerate();
+		
+		 
+
+		
+	}
+}
+
+
